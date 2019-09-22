2f46efbdf5518fcaa8dc63591dc45948ba9e58e0
Jonah Brown <jswish02@gmail.com>
2019-09-22 04:59:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,18 @@
 class Song
   attr_accessor :name, :artist_name
+  def initialize (name, artist_name)
   @@all = []
-
+@name = name
+end
   def self.all
     @@all
   end
-
+def song_create
+  song.create = song
+end
+def new_by_name
+  new_by_name = name
+end
   def save
     self.class.all << self
   end
